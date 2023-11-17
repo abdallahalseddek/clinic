04028f2a0d596fc2509b715a20836f9f0a29b5af
abdallahalseddek <abdallahalseddek@gmail.com>
2023-11-18 00:25:16 +0200

    fixup! add DTO layer

--- src/main/java/com/mangement/clinic/dto/AdminDTO.java
+++ src/main/java/com/mangement/clinic/dto/AdminDTO.java
@@ -1,4 +1,15 @@
 package com.mangement.clinic.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AdminDTO {
+    private String username;
+    private Date createdAt;
 }
